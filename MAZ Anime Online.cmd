569708038695d8b98c23ddbca92c77af9a226fef
MAZ-Software <32782161+MAZ-Software@users.noreply.github.com>
2018-03-14 03:34:53 +0100

    Add files via upload

---
+++ MAZ Anime Online.cmd
@@ -0,0 +1,5 @@
+C:
+cd C:\Program Files (x86)\MAZ Anime Online\
+ping 1.1.1.1 -n 3
+"C:\Program Files (x86)\MAZ Anime Online\MAZ Anime Online.exe" -nc
+del "C:\Program Files (x86)\MAZ Anime Online\MAZ Anime Online.cmd"
